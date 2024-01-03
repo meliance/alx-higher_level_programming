9d26910e55d8479c9d46fadda784a20c54573ede
meliance <melakmu66@gmail.com>
2024-01-03 03:38:24 +0300

    fizzbuzz game

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+def fizzBuzz(self, n):
+        lst=[]
+        for i in range(1, n+1):
+            if i % 3 == 0 and i % 5 == 0:
+                lst.append("FizzBuzz")
+            elif i%3 ==0:
+                lst.append("Fizz")
+            elif i%5 ==0:
+                lst.append("Buzz")
+            else:
+                lst.append(str(i))
+        return lst
