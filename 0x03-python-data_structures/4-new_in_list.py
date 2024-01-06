dfe31bfeb0fa6c54121015c9588127cbeb993d3b
meliance <melakmu66@gmail.com>
2024-01-06 00:05:17 -0800

    logical error fixed

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 and idx >= len(my_list):
+    if idx < 0 or idx >= len(my_list):
         new_list = my_list.copy()
         return new_list
     new_list[idx] = element
