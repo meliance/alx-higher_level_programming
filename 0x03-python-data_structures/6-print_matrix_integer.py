c920e3cb84596981ad08749ea2a01cc237fdc17d
meliance <melakmu66@gmail.com>
2024-01-06 03:15:42 -0800

    Parenthesis errror fixed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col in row:
             print("{:d}".format(col), end='')
-            if col != row(-1):
+            if col != row[-1]:
                 print("", end='')
         print("")
