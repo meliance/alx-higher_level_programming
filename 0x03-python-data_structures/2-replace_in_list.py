ca7981053950ccf5844423226138577cc949d3d6
meliance <melakmu66@gmail.com>
2024-01-05 23:54:10 -0800

    logical error fix

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, index, element):
-    if index < 0 and index >= len(my_list):
+    if index < 0 or index >= len(my_list):
         return my_list
     my_list[index]=element
     return my_list
