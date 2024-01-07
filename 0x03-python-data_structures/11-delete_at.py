f7fac35f654a15f7fa88e2e1ff7e27d0d4107f55
meliance <melakmu66@gmail.com>
2024-01-07 11:10:06 +0300

    error fixed

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -7,4 +7,5 @@ def delete_at(my_list=[], idx=0):
         for i in range(len(my_list)):
             if i != idx:
                 new_list.append(my_list[i])
-        return new_list
+                my_list = new_list
+        return my_list
