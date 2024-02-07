1702aedb10ff9e533e22aff3859eeda70bb1044a
meliance <melakmu66@gmail.com>
2024-02-07 05:22:26 +0300

    finally

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-'''A script that stores arguments to a JSON file.
-usage: ./7-add_item.py ......... ....... ..
-'''
-from sys import argv
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+"""Add all arguments to a Python list and save them to a file."""
+import sys
+from os.path import exists
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-argv.pop(0)
-try:
-    deserialized = load_from_json_file("add_item.json")
-    if deserialized is None:
-        save_to_json_file(argv, "add_item.json")
+    filename = "add_item.json"
+
+    if exists(filename):
+        items = load_from_json_file(filename)
     else:
-        deserialized.extend(argv)
-        save_to_json_file(deserialized, "add_item.json")
-except FileNotFoundError:
-    save_to_json_file(argv, "add_item.json")
+        items = []
+
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, filename)
