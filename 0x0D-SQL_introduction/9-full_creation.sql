5bde27c5b7c401e0fc8d2a2a2a9880efe44df743
meliance <melakmu66@gmail.com>
2024-03-13 15:18:11 +0300

    corrected

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,5 @@
 -- create the secind table with name second_table
-CREATE TABLE IF NOT EXIST 
-`second_table` (`id` INT, 'name` VARCHAR(256), `score` INT);
+CREATE TABLE IF NOT EXIST `second_table` (`id` INT, 'name` VARCHAR(256), `score` INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(1, 'John', 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(2, 'Alex', 3);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(3, 'Bob', 14);
