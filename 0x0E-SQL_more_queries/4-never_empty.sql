87e75319589d43cecb646a760eda70b5327787e2
meliance <melakmu66@gmail.com>
2024-03-14 23:08:30 +0300

    typing error fixed

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- create table id_not_null
-CREATE TABLE IF NOT EXIST id_not_null(id INT DEFAULT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256));
