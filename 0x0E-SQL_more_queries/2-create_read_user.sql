9ade834122aa3bd2e4cdf5bb07691e50171985dc
meliance <melakmu66@gmail.com>
2024-03-14 16:01:10 +0300

    create database and user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- create the database hbtn_od_2 and user_0d_2
+CREATE DATABASE IF NOT EXIST hbtn_0d_2;
+CREATE USER IF NOT EXIST user_0d_2@localhost IDENTIFIED BY 'user_0d_2';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
