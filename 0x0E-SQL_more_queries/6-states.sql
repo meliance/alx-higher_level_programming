7637f455e52eae92f9cd908feb39092f4fa05daf
meliance <melakmu66@gmail.com>
2024-03-14 23:15:17 +0300

    done

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
 -- create the database hbtn_od_usa and table states
-CREATE DATABASE IF NOT EXIST hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_od_usa;
 CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
