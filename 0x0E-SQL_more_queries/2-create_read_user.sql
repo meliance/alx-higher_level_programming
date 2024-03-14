0dceed5f347da5135e9cfbb178a1ec0084c8e015
meliance <melakmu66@gmail.com>
2024-03-14 23:19:06 +0300

    done

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,6 @@
 -- create the database hbtn_od_2 and user_0d_2
-CREATE DATABASE IF NOT EXIST hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- create users
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2';
+-- grants select privilege
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
