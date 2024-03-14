c933db1e3ce679341c5911f4ac2397e0c58a9d11
meliance <melakmu66@gmail.com>
2024-03-14 13:12:33 +0300

    error fixed

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- list all records of the table second_table
-SELECT `score`, `name` FROM `second_table` ORDER BY `score` DESC
+SELECT `score`, `name` FROM `second_table` WHERE `name` != "" ORDER BY `score` DESC
