dd5fe221f81d347694f044cb2d8d47db7bf12792
meliance <melakmu66@gmail.com>
2024-03-15 13:17:41 +0300

    list all shows without genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- lists all shows contained in hbtn_0d_tvshows without a genre linked
+SELECT name FROM tv_genres
+WHERE name NOT IN (SELECT name FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter')
+GROUP BY name ORDER BY name ASC;
