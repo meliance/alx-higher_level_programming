5722fed82cfc5e28934992cf45dae7909340774d
meliance <melakmu66@gmail.com>
2024-03-15 13:32:37 +0300

    done

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,12 +1,8 @@
 -- lists all shows contained in hbtn_0d_tvshows without a genre linked
--- uses a database to list all rows not linked to one row
-SELECT name
-FROM tv_genres
-WHERE name NOT IN
-(SELECT name
-FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+SELECT tv_genres.name FROM tv_genres
+WHERE tv_genres.id NOT IN (
+SELECT tv_genres.id FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_shows.title = 'Dexter')
-GROUP BY name
-ORDER BY name ASC;
+ORDER BY tv_genres.name ASC;
