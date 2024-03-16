8d90da4cbe67f9e03e97dd89b571000b9d755f32
meliance <melakmu66@gmail.com>
2024-03-16 16:45:43 +0300

    fff

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,4 +1,4 @@
---lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+-- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
 SELECT title, name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
