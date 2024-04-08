52caf1cb4bb4bab2f56744639ebf1dbb69a00a2b
meliance <melakmu66@gmail.com>
2024-04-08 20:21:01 +0300

    gdx

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // prints the first argument passed to it
 
-if (process.argv[2] {
-	console.log(process.argv[2]);
+if (process.argv[2] === undefined) {
+  console.log('No argument');
 } else {
-	console.log("No argument");
+  console.log(process.argv[2]);
 }
