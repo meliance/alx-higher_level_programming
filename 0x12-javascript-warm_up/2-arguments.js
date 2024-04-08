9f8c9072945ccc6bdbe08e08cb5d58b8aa23c901
meliance <melakmu66@gmail.com>
2024-04-08 20:14:40 +0300

    done

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-// Print string “Javascript is amazing”
-
-if (process.argv.length == 2) {
-	console.log('No argument found');
-} else if (process.argv.length == 3) {
-	console.log('Argument found');
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
