b65c83964a1efda76dd4e81ed6a406e320a52762
meliance <melakmu66@gmail.com>
2024-04-09 21:48:40 +0300

    fix issues

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,17 +1,21 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-module.exports = class Rectsngle {
-        constructor(w, h) {
-                if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h) {
-                        return 0;
-                } else {
-                        this.width = w;
-                        this.height = h;
-                }
-        }
-	print() {
-		for (let i = 0; i < this.height; i++) {
-			console.log('x'.repeat(this.width));
-		}
-	}
-};
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
+    }
+  }
+}
+
+module.exports = Rectangle;
