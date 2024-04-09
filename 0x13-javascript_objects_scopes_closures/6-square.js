474f951e6bb6d193852d4156e6c69cc8e35df705
meliance <melakmu66@gmail.com>
2024-04-09 21:53:08 +0300

    done

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,19 @@
 #!/usr/bin/node
-module.exports = class Square extends require('./5-square.js') {
+const SquareP = require('./5-square');
+
+class Square extends SquareP {
   charPrint (c) {
     if (c === undefined) {
-      this.print();
-    } else {
-      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
     }
   }
-};
+}
+
+module.exports = Square;
