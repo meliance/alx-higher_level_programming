e83eec6efc51d31fd75c7110150d95ef771a99c6
meliance <melakmu66@gmail.com>
2024-04-09 10:08:44 +0300

    Rectangle #2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+module.exports = class Rectsngle {
+        constructor(w, h) {
+		if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h) {
+			return 0;
+		} else {
+			this.width = w;
+			this.height = h;
+		}
+        }
+};
