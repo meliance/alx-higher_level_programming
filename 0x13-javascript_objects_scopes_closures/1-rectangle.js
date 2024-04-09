a0c469058a05ceace47ee36964b294c2f5151d84
meliance <melakmu66@gmail.com>
2024-04-09 21:44:59 +0300

    error fix

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
 
-module.exports = class Rectsngle {
-	constructor(w, h) {
-		this.width = w;
-		this.height = h;
-	}
-};
+module.exports = Rectangle;
