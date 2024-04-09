46fd09ad129038591b1a1823ee8d84db4ec7cc7b
meliance <melakmu66@gmail.com>
2024-04-09 21:46:56 +0300

    done

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
 
-module.exports = class Rectsngle {
-        constructor(w, h) {
-		if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h) {
-			return 0;
-		} else {
-			this.width = w;
-			this.height = h;
-		}
-        }
-};
+module.exports = Rectangle;
