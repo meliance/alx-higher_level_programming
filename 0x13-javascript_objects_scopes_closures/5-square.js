ee127098327483e546966e0b2e17f1925c68090b
meliance <melakmu66@gmail.com>
2024-04-09 21:51:52 +0300

    error fix

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
+const Rectangle = require('./4-rectangle');
 
-const Rectangle = require('./4-rectangle.js');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
 
-module.exports = class Square extends Rectangle {
-	constructor(size) {
-		 super(size, size);
-	}
-};
+module.exports = Square;
