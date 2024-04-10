5f218b4932cf40c59253c8fac3e986e6d0158a15
meliance <melakmu66@gmail.com>
2024-04-10 17:30:45 +0300

    done

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
+const list = require('./100-data.js').list;
 console.log(list);
-console.log(list.map((x, i) => x * i));
+console.log(list.map((item, index) => item * index));
