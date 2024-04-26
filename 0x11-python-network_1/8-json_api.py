71ba998bf1f2a0fbadf4ce2841e71bce6e9c7885
meliance <melakmu66@gmail.com>
2024-04-26 19:02:47 +0300

    search api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" a python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    response = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        data = response.json()
+        if data:
+            print("[{}] {}".format(data['id'], data['name']))
+        else:
+            print("No result")
+    except ValueError:
+                                                                                                        print("Not a valid JSON")
