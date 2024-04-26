91ade7eda65a8250cec15587a01aad8fe958aa58
meliance <melakmu66@gmail.com>
2024-04-26 17:29:15 +0300

    error fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,10 +6,11 @@ import urllib.request
 
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type:", type(body))
-        print("\t- content:", body)
-        print("\t- utf8 content:", body.decode('utf-8'))
 
+    with urllib.request.urlopen(url) as response:
+        html_content = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(html_content)))
+    print("\t- content: {}".format(html_content))
+    print("\t- utf8 content: {}".format(html_content.decode('utf-8')))
