3fb40be5e383e280eb574e79125d83ca06dd06d3
meliance <melakmu@gmail.com>
2024-04-30 15:09:37 +0300

    error fix

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,2 +1,3 @@
-end a GET request to a given URL with a header variable.
+#!/bin/bash
+# a Bash script that takes in a URL as an argument,end a GET request to a given URL with a header variable.
 curl -sL -H "X-School-User-Id: 98" "$1"
