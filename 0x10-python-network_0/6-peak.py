aea7ba94df0ee40b7f620f371fe26aae48c95056
meliance <melakmu@gmail.com>
2024-04-30 15:35:27 +0300

    updated

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-"""nds a peak in a list of unsorted integers"""
 
 
 def find_peak(list_of_integers):
-        """Finds a peak in list_of_integers"""
 
-            if list_of_integers is None or list_of_integers == []:
-                    return None
-                        lo = 0
-                            hi = len(list_of_integers)
-                                mid = ((hi - lo) // 2) + lo
-                                    mid = int(mid)
-                                        if hi == 1:
-                                                return list_of_integers[0]
-                                                    if hi == 2:
-                                                            return max(list_of_integers)
-                                                                if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-                                                                                    list_of_integers[mid] >= list_of_integers[mid + 1]:
-                                                                                            return list_of_integers[mid]
-                                                                                                if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-                                                                                                        return find_peak(list_of_integers[mid:])
-                                                                                                            if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-                                                                                                                    return find_peak(list_of_integers[:mid])
+    if list_of_integers is None or len(list_of_integers) == 0:
+        return None
+
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+
+    mid_idx = int(len(list_of_integers) / 2)
+
+    if mid_idx != len(list_of_integers) - 1:
+        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx] and\
+           list_of_integers[mid_idx + 1] < list_of_integers[mid_idx]:
+            return list_of_integers[mid_idx]
+    else:
+        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx]:
+            return list_of_integers[mid_idx]
+        else:
+            return list_of_integers[mid_idx - 1]
+
+    if list_of_integers[mid_idx - 1] > list_of_integers[mid_idx]:
+        a_list = list_of_integers[0:mid_idx]
+    else:
+        a_list = list_of_integers[mid_idx + 1:]
+
+    return find_peak(a_list)
